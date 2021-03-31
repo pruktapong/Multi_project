e59d731a0580a5556fbcdf304781ed7cf8ce1a5e
it63070113 <73680313+it63070113@users.noreply.github.com>
2021-03-31 14:55:54 +0700

    Update kkk.js

--- kkk.js
+++ kkk.js
@@ -8,23 +8,26 @@ var button3 = document.getElementById("button3");
 var button4 = document.getElementById("button4");
 var count = 0;
 var topscore = 0;
+
 document.getElementById("restart_btn").style.display = "none";
+document.getElementById("stop_btn").style.display = "none";
+
 function gameplay(x) {
   document.getElementById("type").innerHTML = "Press any Key to play";
   document.addEventListener("keyup", function correct(event) {
     console.log(event.key);
     x = event.key;
 
-    let check1 = parseInt(
+    var check1 = parseInt(
       window.getComputedStyle(object1).getPropertyValue("top")
     );
-    let check2 = parseInt(
+    var check2 = parseInt(
       window.getComputedStyle(object2).getPropertyValue("top")
     );
-    let check3 = parseInt(
+    var check3 = parseInt(
       window.getComputedStyle(object3).getPropertyValue("top")
     );
-    let check4 = parseInt(
+    var check4 = parseInt(
       window.getComputedStyle(object4).getPropertyValue("top")
     );
     if (count > topscore) {
@@ -40,52 +43,52 @@ function gameplay(x) {
       level2();
     }
     if (count == 50) {
+      count++;
       swap();
       alert("Level 3!!!");
       level3();
     }
-    if (count <= -5 || count >= 70) {
+    if (count <= -6 || count >= 70) {
       end();
     }
-    if (x == "a" || x == "s" || x == "d" || x == "f") {
-      if (check1 > -130 && check1 < -30) {
-        console.log("Hit1");
-        document.getElementById("txt").innerHTML = "Hit";
-        count++;
-        document.getElementById("score").innerHTML = count;
-        document.getElementById("type").innerHTML = "ชนิด 1";
-      } else if (check2 > -230 && check2 < -130) {
-        console.log("Hit2");
-        document.getElementById("txt").innerHTML = "Hit";
-        count++;
-        document.getElementById("score").innerHTML = count;
-        document.getElementById("type").innerHTML = "ชนิด 2";
-      } else if (check3 > -330 && check3 < -230) {
-        console.log("Hit3");
-        document.getElementById("txt").innerHTML = "Hit";
-        count++;
-        document.getElementById("score").innerHTML = count;
-        document.getElementById("type").innerHTML = "ชนิด 3";
-      } else if (check4 > -430 && check4 < -330) {
-        console.log("Hit4");
-        document.getElementById("txt").innerHTML = "Hit";
-        count++;
-        document.getElementById("score").innerHTML = count;
-        document.getElementById("type").innerHTML = "ชนิด 4";
-      } else if (
-        check1 > -30 ||
-        check2 > -130 ||
-        check3 > -230 ||
-        check4 > -330
-      ) {
-        console.log("Miss btn");
-        document.getElementById("txt").innerHTML = "Miss";
-        document.getElementById("score").innerHTML = count;
-        count--;
-      }
+    if (x == "a" && check1 > -130 && check1 < -30) {
+      console.log("Hit1");
+      document.getElementById("txt").innerHTML = "Hit";
+      count++;
+      document.getElementById("score").innerHTML = count;
+      document.getElementById("type").innerHTML = "ชนิด 1";
+    } else if (x == "s" && check2 > -230 && check2 < -130) {
+      console.log("Hit2");
+      document.getElementById("txt").innerHTML = "Hit";
+      count++;
+      document.getElementById("score").innerHTML = count;
+      document.getElementById("type").innerHTML = "ชนิด 2";
+    } else if (x == "d" && check3 > -330 && check3 < -230) {
+      console.log("Hit3");
+      document.getElementById("txt").innerHTML = "Hit";
+      count++;
+      document.getElementById("score").innerHTML = count;
+      document.getElementById("type").innerHTML = "ชนิด 3";
+    } else if (x == "f" && check4 > -430 && check4 < -330) {
+      console.log("Hit4");
+      document.getElementById("txt").innerHTML = "Hit";
+      count++;
+      document.getElementById("score").innerHTML = count;
+      document.getElementById("type").innerHTML = "ชนิด 4";
+    } else if (
+      check1 > -30 ||
+      check2 > -130 ||
+      check3 > -230 ||
+      check4 > -330
+    ) {
+      console.log("Miss btn");
+      document.getElementById("txt").innerHTML = "Miss";
+      document.getElementById("score").innerHTML = count;
+      count--;
     }
   });
 }
+
 function countdown() {
   var time = 3;
   var main = setInterval(function () {
@@ -98,18 +101,24 @@ function countdown() {
     }
   }, 1000);
 }
+
 function start() {
-  time = 0;
-  couut = 0;
+  count = 0;
   document.getElementById("time").style.display = "block";
   document.getElementById("type").style.display = "block";
+  document.getElementById("stop_btn").style.display = "block";
+  document.getElementById("start_btn").style.display = "none";
   countdown();
 }
+
 function stop() {
   end();
   alert("Stop the game!");
+  document.getElementById("stop_btn").style.display = "none";
+  document.getElementById("start_btn").style.display = "none";
   document.getElementById("type").style.display = "none";
 }
+
 function end() {
   object1.style.animation = "none";
   object2.style.animation = "none";
@@ -117,18 +126,22 @@ function end() {
   object4.style.animation = "none";
   document.getElementById("txt").innerHTML = "คะแนนสูงสุดของคุณ!!!";
   document.getElementById("score").innerHTML = topscore;
+  document.getElementById("stop_btn").style.display = "none";
   document.getElementById("type").style.display = "none";
   document.getElementById("restart_btn").style.display = "block";
 }
+
 function restart() {
   window.location.reload(true);
 }
+
 function swap() {
   object1.style.animation = "none";
   object2.style.animation = "none";
   object3.style.animation = "none";
   object4.style.animation = "none";
 }
+
 function level1() {
   object1.style.animation = "block1 10s infinite";
   object2.style.animation = "block2 4s infinite";
