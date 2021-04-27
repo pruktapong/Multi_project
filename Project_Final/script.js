813564080e09b9e7884496cd78b86839f74ac13e
taifhoon <73688884+taifhoon@users.noreply.github.com>
2021-04-28 00:24:07 +0700

    Add files via upload

--- Project_Final/script.js
+++ Project_Final/script.js
@@ -1,47 +1,11 @@
- var count = 0;
- var over = 0;
+var count = 0;
+var point = 0;
+var check = 1;
+var block = document.getElementById("block");
 
-function moveLeft() {
-  var left = parseInt(
-    window.getComputedStyle(button).getPropertyValue("left"));
-  left = 0;
-  button.style.left = left + "px";
-
+function start() {
+    block.style.animation = "slide 1.5s infinite";
 }
-function moveRight() {
-  var left = parseInt(
-    window.getComputedStyle(button).getPropertyValue("left"));
-  left = 100;
-  button.style.left = left + "px";
-}
-function moveUp() {
-  var left = parseInt(
-    window.getComputedStyle(button).getPropertyValue("left"));
-  left = 200;
-  button.style.left = left + "px";
-}
-function moveDown() {
-  var left = parseInt(
-    window.getComputedStyle(button).getPropertyValue("left"));
-  left = 300;
-  button.style.left = left + "px";
-}
-
-
-document.addEventListener("keyup", (event) => {
-  if (event.key === "q") {
-    moveLeft();
-  }
-  if (event.key === "e")  {
-    moveUp();
-  }
-  if (event.key === "r") {
-    moveDown();
-  }
-  if (event.key === "w") {
-    moveRight();
-  }
-});
 
 var block = document.getElementById("block");
 block.addEventListener('animationiteration', () => {
@@ -49,34 +13,50 @@ block.addEventListener('animationiteration', () => {
   left = random * 100;
   block.style.left = left + "px";
   console.log(random);
+  count++;
 });
 
-setInterval(() => {
-  var buttonLeft = 
-  parseInt(window.getComputedStyle(button).getPropertyValue("left"));
-  var blockLeft = 
-  parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-  var blockTop = 
-  parseInt(window.getComputedStyle(block).getPropertyValue("top"));
-  if (buttonLeft == blockLeft && blockTop < 500 && blockTop > 300) {
-    count++;
-    console.log("hit");
-    document.getElementById("score").innerHTML = count;
-  }
-  if (count%5 == 0) {
-    console.log("Next Level!!!");
-    clearInterval();
-  }
-  if (count == 6) {
-    document.getElementById("block").style.animation = "slide 1s infinite";
-  }
-  if (count == 21) {
-    document.getElementById("block").style.animation = "slide 0.95s infinite";
-  }
-  if (count == 31) {
-    document.getElementById("block").style.animation = "slide 0.75s infinite";
-  }
-}, 100);
+  document.addEventListener("keyup", (event) => {
+    var element = document.getElementById("block");
+    var check_x = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    var check_y = parseInt(window.getComputedStyle(block).getPropertyValue("top"));
+    if (event.key == "q" && check_x == 0 && check_y <= 600 && check_y > 500) {
+      console.log("hit1");
+      check = 0
+    }
+    else if (event.key === "w" && check_x == 100 && check_y <= 600 && check_y > 500) {
+      console.log("hit2");
+      check = 0
+    }
+    else if (event.key === "e" && check_x == 200 && check_y <= 600 && check_y > 500)  {
+      console.log("hit3");
+      check = 0
+    }
+    else if (event.key === "r" && check_x == 300 && check_y <= 600 && check_y > 500) {
+      console.log("hit4");
+      check = 0
+    }
+    else if (check_y <= 500){
+      console.log("up");
+      point--;
+      element.style.animation = 'none';
+      element.offsetWidth;
+      element.style.animation = 'slide 1.5s infinite';
+      document.getElementById("score").innerHTML = point;
+    }
 
-document.getElementById("left").addEventListener("touchstart", moveLeft);
-document.getElementById("right").addEventListener("touchstart", moveRight);
+
+  });
+  setInterval(() => {
+    var check_y = parseInt(window.getComputedStyle(block).getPropertyValue("top"));
+    if (check == 0 && check_y > 600){
+      console.log("hitttt");
+      check = 1;
+      point++;
+    }
+    else if (check == 1 && check_y > 600){
+      console.log("miss");
+      point--;
+    }
+    document.getElementById("score").innerHTML = point;
+  },500)
