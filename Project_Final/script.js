4ec4a9ba8d464c75246b167e62bd0c93512bc24f
taifhoon <73688884+taifhoon@users.noreply.github.com>
2021-05-22 22:15:44 +0700

    Update script.js

--- Project_Final/script.js
+++ Project_Final/script.js
@@ -23,9 +23,9 @@ function howtoplay() {
 function setTime() {
   setTimeout(function () {
     ++totalSeconds;
-    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    secondsLabel.innerHTML = ": " + pad(totalSeconds % 60);
     minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-  }, 3000);
+  }, 000);
 }
 
 function pad(val) {
@@ -45,7 +45,7 @@ function first() {
   setInterval(setTime, 1000);
   setTimeout(function start() {
     block.style.animation = "slide 1.5s infinite";
-  }, 3000);
+  }, 000);
 }
 block.addEventListener("animationiteration", () => {
   var random_img = Math.floor(Math.random() * 10);
@@ -100,8 +100,10 @@ document.addEventListener("keyup", (event) => {
   );
 
   //console.log(event.key);
+  // 500 - 550
+  //
 
-  if (event.key == "q" && check_x == 0 && check_y <= 600 && check_y > 500) {
+  if (event.key == "q" && check_x == 0 && check_y <= 600 && check_y > 500 && check != 0) {
     //Condition Gameplay(main)
     console.log("hit1");
     point++;
@@ -111,7 +113,8 @@ document.addEventListener("keyup", (event) => {
     event.key === "w" &&
     check_x == 100 &&
     check_y <= 600 &&
-    check_y > 500
+    check_y > 500 &&
+    check != 0
   ) {
     console.log("hit2");
     point++;
@@ -121,7 +124,8 @@ document.addEventListener("keyup", (event) => {
     event.key === "e" &&
     check_x == 200 &&
     check_y <= 600 &&
-    check_y > 500
+    check_y > 500 &&
+    check != 0
   ) {
     console.log("hit3");
     point++;
@@ -131,7 +135,8 @@ document.addEventListener("keyup", (event) => {
     event.key === "r" &&
     check_x == 300 &&
     check_y <= 600 &&
-    check_y > 500
+    check_y > 500 &&
+    check != 0
   ) {
     console.log("hit4");
     point++;
@@ -141,13 +146,14 @@ document.addEventListener("keyup", (event) => {
     event.key === "t" &&
     check_x == 400 &&
     check_y <= 600 &&
-    check_y > 500
+    check_y > 500 &&
+    check != 0
   ) {
     console.log("hit5");
     point++;
     check = 0;
     document.getElementById("score").innerHTML = point;
-  } else if (check_y <= 500 && check != 2) {
+  } else if (check_y > 0 && check_y <= 500 && check != 2) {
     console.log("up");
     live--;
     check = 2;
@@ -158,22 +164,19 @@ document.addEventListener("keyup", (event) => {
 
 var timer = setInterval(() => {
   //Condition Gameplay(Check Miss)
+  console.log(check);
   var check_y = parseInt(
     window.getComputedStyle(block).getPropertyValue("top")
   );
-  if (check == 0 && check_y > 600) {
+  if (check_y < 0) {
     console.log("hitttt");
     check = 1;
-    document.getElementById("score").innerHTML = point;
   } else if (check == 1 && check_y > 600) {
     console.log("miss");
+    check = 3;
     live--;
     document.getElementById("score").innerHTML = point;
     document.getElementById("live").innerHTML = "live : " + live;
-  } else if (check == 2 && check_y > 600) {
-    console.log("upppp");
-    check = 1;
-    document.getElementById("score").innerHTML = point;
   }
   if (live == 10) {
     img.src = "img/ระดับ2.jpg";
@@ -205,7 +208,7 @@ var timer = setInterval(() => {
   if (point == 15 && check_y > 600) {
     block.style.animation = "slide 0.70s infinite";
   }
-}, 550);
+}, 50);
 
 function myStopFunction() {
   clearInterval(setTime);
